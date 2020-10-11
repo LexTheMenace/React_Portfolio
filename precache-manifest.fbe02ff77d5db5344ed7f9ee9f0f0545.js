36db37e17c08f1431f2e0d73040cb7a97cb7329e
LexTheMenace <lexthemenace@gmail.com>
2020-10-11 09:22:48 -0400

    Updates

---
+++ precache-manifest.fbe02ff77d5db5344ed7f9ee9f0f0545.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a11cc5f7ebedfc0e908082219a8955",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "fe40e7a8398d59a7f2cc",
+    "url": "/React_Portfolio/static/css/main.dd93ea16.chunk.css"
+  },
+  {
+    "revision": "38103aaf0a37cc81ff63",
+    "url": "/React_Portfolio/static/js/2.82c06158.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Portfolio/static/js/2.82c06158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe40e7a8398d59a7f2cc",
+    "url": "/React_Portfolio/static/js/main.b2d5d77e.chunk.js"
+  },
+  {
+    "revision": "6f47035444c6a860ca23",
+    "url": "/React_Portfolio/static/js/runtime-main.6c723fb5.js"
+  },
+  {
+    "revision": "6a4a7e99d54da6707e79b68b49628b79",
+    "url": "/React_Portfolio/static/media/Bartender-Book.6a4a7e99.png"
+  },
+  {
+    "revision": "1788dd78b86364272af4a7c4942fa51b",
+    "url": "/React_Portfolio/static/media/TroubleshootingLog.1788dd78.png"
+  },
+  {
+    "revision": "d1ba81ccb3fdec2466619b7b3f807f77",
+    "url": "/React_Portfolio/static/media/me.d1ba81cc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.b39d24771a234b19d0ef748c0df048a6.js"
+  "/React_Portfolio/precache-manifest.fbe02ff77d5db5344ed7f9ee9f0f0545.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
