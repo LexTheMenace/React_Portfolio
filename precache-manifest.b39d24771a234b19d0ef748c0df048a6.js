ae7f5840711944c24e59025b687cac90007539ed
LexTheMenace <lexthemenace@gmail.com>
2020-10-11 09:21:38 -0400

    Updates

---
+++ precache-manifest.b39d24771a234b19d0ef748c0df048a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0b696a45270a542f3123576cf64047",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1fa7a079b6ece963c70e",
+    "url": "/react-portfolio/static/css/main.dd93ea16.chunk.css"
+  },
+  {
+    "revision": "38103aaf0a37cc81ff63",
+    "url": "/react-portfolio/static/js/2.82c06158.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.82c06158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa7a079b6ece963c70e",
+    "url": "/react-portfolio/static/js/main.f0969f17.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "6a4a7e99d54da6707e79b68b49628b79",
+    "url": "/react-portfolio/static/media/Bartender-Book.6a4a7e99.png"
+  },
+  {
+    "revision": "1788dd78b86364272af4a7c4942fa51b",
+    "url": "/react-portfolio/static/media/TroubleshootingLog.1788dd78.png"
+  },
+  {
+    "revision": "d1ba81ccb3fdec2466619b7b3f807f77",
+    "url": "/react-portfolio/static/media/me.d1ba81cc.jpg"
+  }
+]);
